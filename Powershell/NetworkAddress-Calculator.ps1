120dfd9f1cfa460564738a82105fd922fa55ff57
Mike Wheway <taegost@gmail.com>
2022-06-18 18:28:45 -0400

    Cleaned up and simplified code. Script isn't working.

--- Powershell/NetworkAddress-Calculator.ps1
+++ Powershell/NetworkAddress-Calculator.ps1
@@ -9,8 +9,6 @@
 .EXAMPLE
   NetworkAddress-Calculator -ip 192.168.1.1 -subnet 255.255.255.0
   NetworkAddress-Calculator -ip 192.168.1.1 -maskLength 24
-.LASTMODIFIED
-  6/17/2022 by Mike Wheway
 #>
 
 #============================ Initialization Section ============================
@@ -37,46 +35,48 @@ Param
   [ValidateRange(1,31)]
   [Alias("CIDR")]
   [int]$maskLength
-
 )
 
 #============================ Class Section ============================
 class IpDetails
 {
   [IpAddress]$ip
-  [string]$ipBinary = ""
+  hidden [string]$ipBinary = ""
   [IpAddress]$subnet
   hidden [string]$subnetBinary = ""
-  [int]$netbits
-  [string]$networkId
+  [int]$maskLength
+  [IpAddress]$networkId
+  [IpAddress]firstNetworkAddress
+  [IpAddress]lastNetworkAddress
+  [IpAddress]broadCastAddress
 
-  IpDetails([string]$inIp, [string]$inSubnet)
+  IpDetails([IpAddress]$inIp, [IpAddress]$inSubnet)
   {
-    if ($inIp.Contains("/")) # We assume it's CIDR format
-    {
-      $splitIp = $inIp.Split("/")
-      $this.ip = $splitIp[0]
-      $this.netbits = $splitIp[1]
-      $this.subnetBinary = $this.subnetBinary.PadRight(($this.netbits - 1), "1").PadRight(32, "0")
-      $this.subnet = [IpDetails]::BinaryToIp($this.subnetBinary)
-    }
-    else 
-    { 
-      $this.ip = $inIp 
-      $this.subnet = $inSubnet
-      $this.subnetBinary = [IpDetails]::IpToBinary($this.subnet)
-
-      # If the subnet is 0.0.0.0 or 255.255.255.255, we need to adjust the logic.
-      if (($this.subnetBinary.Contains("0")) -and ($this.subnetBinary.Contains("1")))
-        { $this.netBits = $this.subnetBinary.IndexOf("0") }
-      elseif ($this.subnetBinary.Contains("0"))
-        { $this.netBits = 0 }
-      else
-        { $this.netBits = 32 }
-    } # if ($inIp.Contains("/"))
+    $this.ip = $inIp 
     $this.ipBinary = [IpDetails]::IpToBinary($this.ip)
-    $this.networkID = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$this.netBits).padright(32,"0"))
-  } # constructor
+    $this.subnet = $inSubnet
+    $this.subnetBinary = [IpDetails]::IpToBinary($this.subnet)
+    $this.maskLength = $this.subnetBinary.IndexOf("0")
+    [IpDetails]::CalculateNetworkInformation()
+  } # subnet constructor
+
+  IpDetails([IpAddress]$inIp, [int]$maskLength)
+  {
+    $this.ip = $inIp 
+    $this.ipBinary = [IpDetails]::IpToBinary($this.ip)
+    $this.maskLength = $maskLength
+    $this.subnetBinary = [string].PadRight(($this.maskLength - 1), "1").PadRight(32, "0")
+    $this.subnet = [IpDetails]::BinaryToIp($this.subnetBinary)
+    [IpDetails]::CalculateNetworkInformation()
+  } # CIDR constructor
+
+  private [void]CalculateNetworkInformation()
+  {
+    $this.networkID = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$this.maskLength).padright(32,"0"))
+    $this.firstNetworkAddress = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$bits).padright(31,"0") + "1")
+    $this.lastNetworkAddress = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$bits).padright(31,"1") + "0")
+    $this.broadCastAddress = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$bits).padright(32,"1"))
+  } # method CalculateNetworkInformation
 
   static [string]IpToBinary ([IpAddress]$ipAddress)
   {
@@ -91,26 +91,7 @@ class IpDetails
     do {$ipAddress += "." + [string]$([convert]::toInt32($binary.substring($i,8),2)); $i+=8 } while ($i -le 24)
     return $ipAddress.substring(1)
   } # method BinaryToIp
-  
-  [string]ToString()
-  {
-    $bits = $this.netbits #Using this.netbits directly causes problems in the return
-
-    # Identify subnet boundaries
-    
-    $firstAddress = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$bits).padright(31,"0") + "1")
-    $lastAddress = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$bits).padright(31,"1") + "0")
-    $broadCast = [IpDetails]::BinaryToIp($this.ipBinary.substring(0,$bits).padright(32,"1"))
-
-    $returnStr =  "Network ID:`t$this.networkID/$bits
-First Address:`t$firstAddress
-Last Address:`t$lastAddress
-Broadcast:`t$broadCast"
-    return $returnStr
-  } # method ShowNetworkBoundaries
 } # class IpDetails
 
 #============================ Main Section ============================
-[IpDetails]$validator = [IpDetails]::new($ip, $subnet)
-$validator
-$validator.ToString()
+[IpDetails]::new($ip, $subnet)
